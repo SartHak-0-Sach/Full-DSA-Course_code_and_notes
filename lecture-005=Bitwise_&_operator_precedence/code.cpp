60d9330ab7cca4c7a1eda2dc4193ba35158b72fb
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-05 17:05:19 +0530

    Update code.cpp

--- lecture-005=Bitwise_&_operator_precedence/code.cpp
+++ lecture-005=Bitwise_&_operator_precedence/code.cpp
@@ -62,6 +62,93 @@ void FifthQuestion()
     cout << c << "\n"; // Output= 1 3
 }
 
+void SixthQuestion()
+{
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << i << " ";
+        i++;
+    }
+    // Output= 0 2 4
+}
+
+void SeventhQuestion()
+{
+    for (int i = 0; i <= 5; i--)
+    {
+        cout << i << " ";
+        i++;
+    }
+    // Output= {infinite loop}
+}
+
+void EighthQuestion()
+{
+    for (int i = 0; i <= 15; i += 2)
+    {
+        cout << i << " ";
+        if (i & 1)
+            continue;
+
+        i++;
+    }
+    // Output= 0 3 5 7 9 11 13 15
+}
+
+void NinthQuestion()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i; j <= 5; j++)
+        {
+            cout << i << " " << j << endl;
+        }
+    }
+    // Output= 0 0 0 1 0 2 0 3 0 4 0 5 1 1 1 2 1 3 1 4 1 5 2 2 2 3 2 4 2 5 3 3 3 4 3 5 4 4 4 5
+}
+
+void TenthQuestion()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i; j <= 5; j++)
+        {
+            if (i + j == 10)
+                break;
+
+            cout << i << " " << j << endl;
+        }
+    }
+    // Output= 0 0 0 1 0 2 0 3 0 4 0 5 1 1 1 2 1 3 1 4 1 5 2 2 2 3 2 4 2 5 3 3 3 4 3 5 4 4 4 5
+}
+
+int subtractProductAndSum(int n)
+{
+    int product = 1;
+    int sum = 0;
+    while(n>0)
+    {
+        int digit = n % 10;
+        product *= digit;
+        sum += digit;
+        n /= 10;
+    }
+    return product-sum;
+}
+
+int hammingWeight(uint32_t n)
+{
+    int setBits = 0;
+    while(n>0)
+    {
+        if(n&1)
+        setBits++;
+
+        n>>1;
+    }
+    return setBits;
+}
+
 int main(int argc, char const *argv[])
 {
     FirstQuestion();
@@ -69,5 +156,10 @@ int main(int argc, char const *argv[])
     ThirdQuestion();
     FourthQuestion();
     FifthQuestion();
+    SixthQuestion();
+    SeventhQuestion();
+    EighthQuestion();
+    NinthQuestion();
+    TenthQuestion();
     return 0;
 }
