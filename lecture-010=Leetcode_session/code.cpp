0831f5c6f9df26b22e4f3bca55a4bcff325e73c5
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-09 19:32:12 +0530

    Update code.cpp

---
+++ lecture-010=Leetcode_session/code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// int findUnique(int *arr, int size)
+// {
+//     int unique = 0;
+//     for (int i = 0; i < size; i++)
+//         unique ^= arr[i];
+//     return unique;
+// }
+
+void printArray(int* arr[])
+{
+    int size = sizeof(arr)/sizeof(int);
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
