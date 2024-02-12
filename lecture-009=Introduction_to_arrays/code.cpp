885fc6cdfd042c2ce20e5099cfe2ff332e50dbde
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-12 21:37:50 +0530

    Update code.cpp

--- lecture-009=Introduction_to_arrays/code.cpp
+++ lecture-009=Introduction_to_arrays/code.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void reverseAnArray(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    while (start < end)
+        swap(arr[start++], arr[end--]);
+}
+
 int main(int argc, char const *argv[])
 {
-    
+    int evenArr[10] = {1, 3, 5, 6, 32, 4, 45, 56, 54, 423};
+    int oddArr[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    reverseAnArray(evenArr, 10);
+    reverseAnArray(oddArr, 11);
+    for (int i = 0; i < 10; i++)
+        cout << evenArr[i] << " " << oddArr[i] << endl;
     return 0;
 }
