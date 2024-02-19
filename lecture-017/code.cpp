15a3573550e856442a5eeba3a526995128ddb4bd
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-19 17:44:41 +0530

    Update code.cpp

--- lecture-017/code.cpp
+++ lecture-017/code.cpp
@@ -1,1 +1,21 @@
-// Add bubble sort code and check if there are optimizations or variations possible
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int> &arr, int n)
+{
+    bool swapped = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swapped = true;
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+        if (swapped == false) // optimization
+            break;
+    }
+}
