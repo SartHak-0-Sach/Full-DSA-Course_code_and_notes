71b53d1506ad8062841d79fa53f24e12efe84622
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-20 22:46:20 +0530

    Update code.cpp

--- lecture-014/code.cpp
+++ lecture-014/code.cpp
@@ -60,6 +60,60 @@ int searchRotatedArray(vector<int> &arr, int key)
     return -1;
 }
 
+int findPivot(vector<int> &arr, int n) {
+    int start = 0;
+    int end = n - 1;
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] > arr[end])
+            start = mid + 1;
+        else
+            end = mid;
+    }
+    return start;
+}
+
+int binarySearch(vector<int>& arr, int start, int end, int k) {
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == k)
+            return mid;
+        else if (arr[mid] < k)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return -1;
+}
+
+int search(vector<int>& arr, int n, int k) {
+    int pivot = findPivot(arr, n);
+    if (k >= arr[pivot] && k <= arr[n - 1])
+        return binarySearch(arr, pivot, n - 1, k);
+    else
+        return binarySearch(arr, 0, pivot - 1, k);
+}
+
+int floorSqrt(int n)
+{
+    long long int start = 0;
+    long long int end = n;
+    while(start <= end)
+    {
+        long long int mid = start + (end-start)/2;
+
+        if(mid*mid == n)
+            return mid;
+
+        if(mid*mid > n)
+            end = mid-1;
+
+        else
+            start = mid+1;
+    }
+    return start-1;
+}
+
 int main(int argc, char const *argv[])
 {
     // vector<int> elements = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5};
