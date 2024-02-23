e678f3d7a11fbbc260ec8fa5772d35afc5667822
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-23 23:01:08 +0530

    Update code.cpp

--- lecture-019=STL_in_one_video/code.cpp
+++ lecture-019=STL_in_one_video/code.cpp
@@ -49,7 +49,131 @@ void vectorImplementation()
     cout << "Size of vector:- " << v.size() << endl;
 
     v.push_back(3);
-    cout << "Capacity of vector:- " << v.capacity() << "\n"; //since we did shrink to fit above, a new array will be allocated to fit these variables of larger size and old array will be deleted
+    cout << "Size of vector:- " << v.size() << endl;
+    cout << "Capacity of vector:- " << v.capacity() << "\n"; // since we did shrink to fit above, a new array will be allocated to fit these variables of larger size and old array will be deleted
 
-    
+    cout << "front element of vector:-" << v.front() << endl;
+    cout << "back element of vector:-" << v.back() << endl;
+
+    // before pop
+    for (auto i : v)
+        cout << i << " ";
+
+    v.pop_back();
+
+    // after pop
+    for (auto i : v)
+        cout << i << " ";
+
+    v.clear();
+    cout<<endl;
+
+    // after clearing the array
+    for (auto i : v)
+        cout << i << " ";
+
+    int m = 3;
+    int n = 2;
+
+    cout<<"\n";
+
+    vector<int> v2(m, n);
+
+    for (auto i : v2)
+        cout << i << " ";
+
+    vector<int> v3 (v2); // can also copy vectors like this as directly equating 2 arrays is not possible
+
+    cout<<"\nVector V3 is:-\n";
+    for (auto i : v3)
+        cout << i << " ";
 }
+
+void dequeImplementation()
+{
+    deque<int> d;
+
+    d.push_back(2);
+    d.push_front(1);
+
+    for (auto i : d)
+        cout << i << " ";
+
+    cout<<endl;
+
+    d.pop_front();
+    d.pop_back();
+
+    d.push_back(3);
+    d.push_front(2);
+    d.push_back(5);
+    d.push_front(1);
+
+    cout<<"Print index of element 1:- "<<d.at(1)<<endl;
+
+    cout<<"front element:- "<<d.front()<<"\n";
+    cout<<"back element:- "<<d.back()<<"\n";
+
+    cout<<"Empty or not:- "<<d.empty()<<endl;
+
+    // d.emplace_back();
+    // d.emplace_front();
+    // d.emplace(3, 4); // add documentation of these commands also
+
+}
+
+// add meaning of this below mentioned code also
+
+/*
+#include <iostream>
+#include <string>
+#include <deque>
+ 
+struct A
+{
+    std::string s;
+ 
+    A(std::string str) : s(std::move(str)) { std::cout << " constructed\n"; }
+ 
+    A(const A& o) : s(o.s) { std::cout << " copy constructed\n"; }
+ 
+    A(A&& o) : s(std::move(o.s)) { std::cout << " move constructed\n"; }
+ 
+    A& operator=(const A& other)
+    {
+        s = other.s;
+        std::cout << " copy assigned\n";
+        return *this;
+    }
+ 
+    A& operator=(A&& other)
+    {
+        s = std::move(other.s);
+        std::cout << " move assigned\n";
+        return *this;
+    }
+};
+ 
+int main()
+{
+    std::deque<A> container;
+ 
+    std::cout << "construct 2 times A:\n";
+    A two { "two" };
+    A three { "three" };
+ 
+    std::cout << "emplace:\n";
+    container.emplace(container.end(), "one");
+ 
+    std::cout << "emplace with A&:\n";
+    container.emplace(container.end(), two);
+ 
+    std::cout << "emplace with A&&:\n";
+    container.emplace(container.end(), std::move(three));
+ 
+    std::cout << "content:\n";
+    for (const auto& obj : container)
+        std::cout << ' ' << obj.s;
+    std::cout << '\n';
+}
+*/
